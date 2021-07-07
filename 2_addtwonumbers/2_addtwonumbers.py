dc69d999df9e3d494212cf50aff26f778130d7f0
spaceduck <spaceduck@Binhs-MacBook-Pro.local>
2021-07-06 21:02:32 -0500

    creating linked list

--- 2_addtwonumbers/2_addtwonumbers.py
+++ 2_addtwonumbers/2_addtwonumbers.py
@@ -1,11 +1,41 @@
+# You are given two non-empty linked lists representing two non-negative integers. 
+# The digits are stored in reverse order, and each of their nodes contains a single digit. 
+# Add the two numbers and return the sum as a linked list.
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+'''
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+'''
 class ListNode(object):
     def __init__(self, val=0,next = None):
         self.val = val
         self.next = next
 
+def getLength(nodesList):
+    curr = nodesList
+    length = 0
+    
+    while (curr != None):
+        length += 1
+        curr = curr.next
+
+    return length
+
+def getNumbersList(length, nodesList):
+    numbersList = [None] * length
+    i = length-1
+    curr = nodesList
+    
+    while (curr != None):
+        numbersList[i] = curr.val
+        i -= 1
+        curr = curr.next
+        
+
 def main():
 
-    
+
     node1 = ListNode(val=1)
     node2 = ListNode(val=2)
     node3 = ListNode(val=3) 
@@ -18,6 +48,7 @@ def main():
     # while (curr != None):
     #     print('val:{} node:{}'.format(curr.val,curr.next))
     #     curr = curr.next
+
     curr = node1
     length = 0
     while (curr != None):
@@ -38,6 +69,9 @@ def main():
         strnum += str(num)
     
     print (strnum)
+
+
+
 
 
     
---
+++ 2_addtwonumbers/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(){}
+    public ListNode(int val) {this.val = val;}
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    ListNode node
+}
